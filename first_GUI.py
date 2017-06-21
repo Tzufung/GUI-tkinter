b0765b4ff18a55be5b24c0ba51a67d2669fea0cb
Tzufung <zfchen1314@gmail.com>
2017-06-21 21:11:54 +0800

    Add a title

---
+++ first_GUI.py
@@ -0,0 +1,6 @@
+# Creating first Python GUI
+import tkinter as tk
+
+win = tk.Tk()    # turn TK class into an instance
+win.title("Python GUL")    # win is short for window; set one property
+win.mainloop()    # start the window's event loop
