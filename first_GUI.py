7301e3f4d2bd4b12250841c97d0f18a417cafd70
Tzufung <zfchen1314@gmail.com>
2017-06-21 21:13:58 +0800

    Disable resizing the GUI

--- first_GUI.py
+++ first_GUI.py
@@ -2,5 +2,8 @@
 import tkinter as tk
 
 win = tk.Tk()    # turn TK class into an instance
+
 win.title("Python GUL")    # win is short for window; set one property
+win.resizable(0, 0)    # Disable resizing the GUL
+
 win.mainloop()    # start the window's event loop
